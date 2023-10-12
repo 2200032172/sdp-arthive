f9005ff2b6f71c853d4a81ee584546897eea972b
2200032172 <2200032172@kluniversity.in>
2023-10-12 23:04:34 +0530

    register page

--- sdp/src/components/Register.js
+++ sdp/src/components/Register.js
@@ -1,68 +1,105 @@
 import './Register.css';
 import React, { useState } from 'react';
-
-
-function Register() {
+import img from './img.png';
+import { Link } from 'react-router-dom';
+const Register = () => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
+    
     email: '',
     password: '',
-    confirmPassword: '',
   });
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Add form submission logic here, e.g., validation and sending data to the server
+    // Add your validation logic here
+
+    // Example: Check if required fields are not empty
+    if (!formData.username || !formData.email || !formData.password) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    // Example: Send data to server or perform signup logic
+    console.log('User Signed Up!', formData);
+    // You can add further logic here, like making an API call to register the user
   };
 
   return (
-    <div className="register">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="firstName"
-          placeholder="First Name"
-          value={formData.firstName}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Last Name"
-          value={formData.lastName}
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email Address"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-        />
-        <button type="submit">Register</button>
-      </form>
+    <div className="class">
+      <div className="left-side">
+        <img src={img} alt="Logo" className="logo" />
+      </div>
+      <div className="right-side">
+      <div className="top-right-links">
+        <a href="/about" className="navbar-link">About Us</a>
+        <span className="navbar-separator"></span>
+        <a href="/faqs" className="navbar-link">FAQs</a>
+        <span className="navbar-separator"></span>
+        <a href="/contact" className="navbar-link">Contact</a>
+      </div>
+        <div className='s'>
+          <h2>  Sign-In here to Enter the Hive </h2>
+        </div>
+       <div className='signup'>
+        <form onSubmit={handleSubmit}>
+         
+          <br />
+
+          <label>
+            Email:
+            <br />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+     
+
+          <label>
+      <span>Password:</span>
+      <br />
+      <input
+        type={showPassword ? 'text' : 'password'}
+        name="password"
+        placeholder="Enter your password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <span
+        style={{ cursor: 'pointer', marginLeft: '25%' }}
+        onClick={() => setShowPassword(!showPassword)}
+      >
+        {showPassword ? '🔓' : '🔒'}
+      </span>
+    </label>
+          <a href="/forgotpassowrd">Forgot Password?</a>
+          <br/>
+          <div className='l'>
+  <Link to='/frontpage' className='button-link'>Login</Link>
+</div>
+
+        </form>
+        <br />
+        <p3>
+            New here? <a href="/register">Register now</a>
+          </p3>
+        </div>
+       
+      </div>
     </div>
   );
-}
+};
 
 export default Register;
